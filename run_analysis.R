bf5e2b5e96ebf32ef718a619023b9477f08eba07
MGreels <mgreeley@gmail.com>
2019-03-22 14:50:52 -0400

    Updated run_analysis and Fulldata.txt

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,10 @@
 # Data Cleaning Script for accelerometer testing data
 # 30 Users performing 6 different activities
 #each instance contains 561 activity accelerometer readings.
-# this script combines test and training data and adds appropriate labels to the data.
-# See Data
+# this script combines test and training data, selects only 
+#means and sds and adds appropriate labels to the data.
+# return is a DF with 180 rows and 88 columns (86 operations)
+
 
 library(dplyr)
 
@@ -27,26 +29,28 @@ acts <- read.table("activity_labels.txt")
 fulldata <- tbl_df(bind_rows( bind_cols(testsub, testy, testx), bind_cols(trainsub, trainy, trainx)))
 rm(trainsub, trainx, trainy, testy, testx, testsub) #removes raw datasets from env
 
-#####   Step 2 - Extracts only the measurements on the mean and SD of each measurement
-        #extracts mean and std data using Regex
-meandata <- fulldata[,grep("*.[Mm]ean*.", featurevect)]
-stddata <- fulldata[grep("*.[Ss]td*.", featurevect)]
+#####   Step 2 - Extracts only the measurements on the mean and SD of each measurement 
+        #extracts mean and std data column locations using Regex, then selects only those columns from fulldata
+meansdlocs <- grep("*.[Mm]ean*.|*.[Ss]td*.", featurevect)
+meandata <- fulldata[ ,c(1,2,meansdlocs+2)]
+rm(fulldata)
+
 
 #####   Step 3 - assigns descriptive names to the activities
         #swaps out integer values with char desciptions
-fulldata <- mutate(fulldata, V1100 = acts$V2[V1100]) 
+meandata <- mutate(meandata, V1100 = acts$V2[V1100]) 
 rm(acts) #remove acts vector from env
 
 #####   Step 4 - Assigns descriptive names to feature columns
         #assigns feature names to DF columns
-colnames(fulldata) <- c("userid", "activity", featurevect) 
+colnames(meandata) <- c("userid", "activity", featurevect[meansdlocs]) 
 
 #####   Step 5 - create new tidy data set with only the mean of all values for every unique User-Activity pair
         #groups DF by userid-activity pair, then takes the mean of 561 acceleration vals
-Means <- fulldata %>%
+Means <- meandata %>%
                 group_by(userid, activity) %>%
                 summarise_all(mean) 
-rm(meandata)
+rm(meandata, featurevect, meansdlocs)
 
         # Write final table of Mean values for 180 userid-activity pairs "
 write.table(Means, file = "Fulldata.txt")
